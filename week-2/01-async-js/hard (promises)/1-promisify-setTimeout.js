acb1cf7d926ff704cdc3a6ccf2472283009e5093
vaibhav7000 <vchawla7000@gmail.com>
2025-05-15 21:43:10 +0530

    some week 2 problems

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,6 +3,11 @@
 */
 
 function wait(n) {
+    return new Promise(function(resolve) {
+        // resolve gets mapped with the function that we pass inside .then 
+        // hence calling resolve makes the actual function call
+        setTimeout(resolve, n * 1000);
+    })
 }
 
 module.exports = wait;
--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,45 @@
  */
 
 function wait1(t) {
-
+    return new Promise(function(resolve) {
+        const beforeTime = Date.now(); // static method of Date class
+        setTimeout(function() {
+            const afterTime = Date.now(); // static method of Date class
+            resolve(afterTime - beforeTime); // pass the time it take to get resolved
+        }, t * 1000);
+    })
 }
 
 function wait2(t) {
-
+    return new Promise(function(resolve) {
+        const beforeTime = Date.now();
+        setTimeout(function() {
+            const afterTime = Date.now();
+            resolve(afterTime - beforeTime);
+        }, t * 1000);
+    })
 }
 
 function wait3(t) {
-
+    return new Promise(function(resolve) {
+        const beforeTime = Date.now();
+        setTimeout(function() {
+            const afterTime = Date.now();
+            resolve(afterTime - beforeTime);
+        }, t * 1000);
+    })
 }
 
 function calculateTime(t1, t2, t3) {
+    // promise.all is used when we want to run multiple promise parallelly and wait until all the promise are resolved
+    // promise.all also returns a promise that will be resolved when all the promises provided to it will be resolved
+    // takes array of promise and then return the resolve value if present in the same orther
+    return Promise.all([wait1(t1), wait2(t2), wait3(t3)]).then(function(timeArray) {
+        
+        const max = Math.max(...timeArray);
 
+        return max;
+    })
 }
 
 module.exports = calculateTime;
